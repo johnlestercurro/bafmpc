a247905c29bb553a7d29b4868b44982d9017d7d5
johnlestercurro <johnlestercurro@gmail.com>
2023-01-25 17:47:07 +0800

    Frontend Finalization Period

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -20,7 +20,7 @@
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
             <div class="container">
-                <a class="navbar-brand" href="#page-top"><img src="assets/img/navbar-logo.svg" alt="..." /></a>
+                <a class="navbar-brand" href="#page-top"><img src="assets/img/navbar-logo.png" alt="..." /></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu
                     <i class="fas fa-bars ms-1"></i>
@@ -245,47 +245,7 @@
         <section class="page-section bg-light" id="team">
             <div class="container">
                 <div class="text-center">
-                    <h2 class="section-heading text-uppercase">FARMCOOP BOARD OF TRUSTEES & OFFICERS</h2>
-                    <h3 class="section-subheading text-muted">2022-2025.</h3>
-                </div>
-                <div class="row">
-                    <div class="col-lg-4">
-                        <div class="team-member">
-                            <img class="mx-auto rounded-circle" src="assets/img/team/1.jpg" alt="..." />
-                            <h4>Sara Duterte</h4>
-                            <p class="text-muted">Chairperson</p>
-                            <a class="btn btn-dark btn-social mx-2" href="https://twitter.com/indaysara" aria-label="Sara Duterte Twitter Profile"><i class="fab fa-twitter"></i></a>
-                            <a class="btn btn-dark btn-social mx-2" href="https://www.facebook.com/MayorIndaySaraDuterteOfficial/" aria-label="Sara Duterte Facebook Profile"><i class="fab fa-facebook-f"></i></a>
-                            <a class="btn btn-dark btn-social mx-2" href="https://www.tiktok.com/@love_inday_sara" aria-label="Sara Duterte Tiktok Profile"><i class="fab fa-tiktok"></i></a>
-                        </div>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="team-member">
-                            <img class="mx-auto rounded-circle" src="assets/img/team/2.jpg" alt="..." />
-                            <h4>Frankirico Restor</h4>
-                            <p class="text-muted">Vice Chairperson</p>
-                            <a class="btn btn-dark btn-social mx-2" href="https://www.facebook.com/Frankirico.ph" aria-label="Frankirico Restor Facebook Profile"><i class="fab fa-facebook-f"></i></a>
-                            <a class="btn btn-dark btn-social mx-2" href="https://www.youtube.com/@baphometfrombxu9549" aria-label="Frankirico Restor Youtube Profile"><i class="fab fa-youtube"></i></a>
-                            <a class="btn btn-dark btn-social mx-2" href="https://github.com/BAPHOMETfromBXU/" aria-label="Frankirico Restor Github Profile"><i class="fab fa-github"></i></a>
-                        </div>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="team-member">
-                            <img class="mx-auto rounded-circle" src="assets/img/team/3.jpg" alt="..." />
-                            <h4>John Lester Curro</h4>
-                            <p class="text-muted">Secretary</p>
-                            <a class="btn btn-dark btn-social mx-2" href="https://www.facebook.com/johnlester.curro" aria-label="John Lester Curro Facebook Profile"><i class="fab fa-facebook-f"></i></a>
-                            <a class="btn btn-dark btn-social mx-2" href="https://www.instagram.com/johnlestercurro20/" aria-label="John Lester Curro Instagram Profile"><i class="fab fa-instagram"></i></a>
-                            <a class="btn btn-dark btn-social mx-2" href="https://github.com/johnlestercurro/" aria-label="John Lester Curro Github Profile"><i class="fab fa-github"></i></a>
-                        </div>
-                    </div>
-                </div>
-        </section>
-        <!-- Team2-->
-        <section class="page-section bg-light" id="team">
-            <div class="container">
-                <div class="text-center">
-                    <h2 class="section-heading text-uppercase">FARMCOOP TEAM AND TEAM CONTRIBUTIONS</h2>
+                    <h2 class="section-heading text-uppercase">BOARD OF TRUSTEES</h2>
                     <h3 class="section-subheading text-muted">2022-2025.</h3>
                 </div>
                 <div class="row">
@@ -321,79 +281,78 @@
                     </div>
                 </div>
         </section>
+        <!-- Team2-->
+        <section class="page-section bg-light" id="team">
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="section-heading text-uppercase">SOCIAL MEDIA TEAM</h2>
+                    <h3 class="section-subheading text-muted">2022-2025.</h3>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="team-member">
+                            <img class="mx-auto rounded-circle" src="assets/img/team/2.jpg" alt="..." />
+                            <h4>Frankirico Restor</h4>
+                            <p class="text-muted">Lead Designer</p>
+                            <a class="btn btn-dark btn-social mx-2" href="https://www.facebook.com/Frankirico.ph" aria-label="Frankirico Restor Facebook Profile"><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-dark btn-social mx-2" href="https://www.youtube.com/@baphometfrombxu9549" aria-label="Frankirico Restor Youtube Profile"><i class="fab fa-youtube"></i></a>
+                            <a class="btn btn-dark btn-social mx-2" href="https://github.com/BAPHOMETfromBXU/" aria-label="Frankirico Restor Github Profile"><i class="fab fa-github"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="team-member">
+                            <img class="mx-auto rounded-circle" src="assets/img/team/3.jpg" alt="..." />
+                            <h4>John Lester Curro</h4>
+                            <p class="text-muted">Lead Marketer</p>
+                            <a class="btn btn-dark btn-social mx-2" href="https://www.facebook.com/johnlester.curro" aria-label="John Lester Curro Facebook Profile"><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-dark btn-social mx-2" href="https://www.instagram.com/johnlestercurro20/" aria-label="John Lester Curro Instagram Profile"><i class="fab fa-instagram"></i></a>
+                            <a class="btn btn-dark btn-social mx-2" href="https://github.com/johnlestercurro/" aria-label="John Lester Curro Github Profile"><i class="fab fa-github"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="team-member">
+                            <img class="mx-auto rounded-circle" src="assets/img/team/1.jpg" alt="..." />
+                            <h4>Sara Duterte</h4>
+                            <p class="text-muted">Lead Developer</p>
+                            <a class="btn btn-dark btn-social mx-2" href="https://twitter.com/indaysara" aria-label="Sara Duterte Twitter Profile"><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-dark btn-social mx-2" href="https://www.facebook.com/MayorIndaySaraDuterteOfficial/" aria-label="Sara Duterte Facebook Profile"><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-dark btn-social mx-2" href="https://www.tiktok.com/@love_inday_sara" aria-label="Sara Duterte Tiktok Profile"><i class="fab fa-tiktok"></i></a>
+                        </div>
+                    </div>
+                </div>
+        </section>
         <!-- Contact-->
         <section class="page-section" id="contact">
             <div class="container">
                 <div class="text-center">
-                    <h2 class="section-heading text-uppercase">Contact Us</h2>
-                    <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                    <h2 class="section-heading text-uppercase">For more inquires Contact our Official Number: 09485554255</h2>
+                    <h3 class="section-heading text-uppercase">Address: Purok 10, Los Angeles, Butuan City</h3
                 </div>
-                <!-- * * * * * * * * * * * * * * *-->
-                <!-- * * SB Forms Contact Form * *-->
-                <!-- * * * * * * * * * * * * * * *-->
-                <!-- This form is pre-integrated with SB Forms.-->
-                <!-- To make this form functional, sign up at-->
-                <!-- https://startbootstrap.com/solution/contact-forms-->
-                <!-- to get an API token!-->
-                <form id="contactForm" data-sb-form-api-token="API_TOKEN">
-                    <div class="row align-items-stretch mb-5">
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <!-- Name input-->
-                                <input class="form-control" id="name" type="text" placeholder="Your Name *" data-sb-validations="required" />
-                                <div class="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
-                            </div>
-                            <div class="form-group">
-                                <!-- Email address input-->
-                                <input class="form-control" id="email" type="email" placeholder="Your Email *" data-sb-validations="required,email" />
-                                <div class="invalid-feedback" data-sb-feedback="email:required">An email is required.</div>
-                                <div class="invalid-feedback" data-sb-feedback="email:email">Email is not valid.</div>
-                            </div>
-                            <div class="form-group mb-md-0">
-                                <!-- Phone number input-->
-                                <input class="form-control" id="phone" type="tel" placeholder="Your Phone *" data-sb-validations="required" />
-                                <div class="invalid-feedback" data-sb-feedback="phone:required">A phone number is required.</div>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="form-group form-group-textarea mb-md-0">
-                                <!-- Message input-->
-                                <textarea class="form-control" id="message" placeholder="Your Message *" data-sb-validations="required"></textarea>
-                                <div class="invalid-feedback" data-sb-feedback="message:required">A message is required.</div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Submit success message-->
-                    <!---->
-                    <!-- This is what your users will see when the form-->
-                    <!-- has successfully submitted-->
-                    <div class="d-none" id="submitSuccessMessage">
-                        <div class="text-center text-white mb-3">
-                            <div class="fw-bolder">Form submission successful!</div>
-                            To activate this form, sign up at
-                            <br />
-                            <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
-                        </div>
-                    </div>
-                    <!-- Submit error message-->
-                    <!---->
-                    <!-- This is what your users will see when there is-->
-                    <!-- an error submitting the form-->
-                    <div class="d-none" id="submitErrorMessage"><div class="text-center text-danger mb-3">Error sending message!</div></div>
-                    <!-- Submit Button-->
-                    <div class="text-center"><button class="btn btn-primary btn-xl text-uppercase disabled" id="submitButton" type="submit">Send Message</button></div>
-                </form>
+            </div>
+            <center>
+                    <!-- Google Map Copied Code -->
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d985.1492171767253!2d125.60627247688376!3d9.009190593600472!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sph!4v1674635946994!5m2!1sen!2sph"
+                            width="400"
+                            height="300"
+                            frameborder="0"
+                            style="border:0;"
+                            allowfullscreen=""
+                            aria-hidden="false"
+                            tabindex="0">
+                    </iframe>
+                </div>
+            </center>
             </div>
         </section>
         <!-- Footer-->
         <footer class="footer py-4">
             <div class="container">
                 <div class="row align-items-center">
-                    <div class="col-lg-4 text-lg-start">Copyright &copy; Your Website 2022</div>
+                    <div class="col-lg-4 text-lg-start">Copyright &copy; BAFMPC 2023</div>
                     <div class="col-lg-4 my-3 my-lg-0">
-                        <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Twitter"><i class="fab fa-twitter"></i></a>
-                        <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Facebook"><i class="fab fa-facebook-f"></i></a>
-                        <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="LinkedIn"><i class="fab fa-linkedin-in"></i></a>
+                        <a class="btn btn-dark btn-social mx-2" href="https://www.facebook.com/johnlester.curro" aria-label="John Lester Curro Facebook Profile"><i class="fab fa-facebook-f"></i></a>
+                        <a class="btn btn-dark btn-social mx-2" href="https://www.instagram.com/johnlestercurro20/" aria-label="John Lester Curro Instagram Profile"><i class="fab fa-instagram"></i></a>
+                        <a class="btn btn-dark btn-social mx-2" href="https://github.com/johnlestercurro/" aria-label="John Lester Curro Github Profile"><i class="fab fa-github"></i></a>
                     </div>
                     <div class="col-lg-4 text-lg-end">
                         <a class="link-dark text-decoration-none me-3" href="#!">Privacy Policy</a>
